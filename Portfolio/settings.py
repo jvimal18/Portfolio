109d046cb2d1ee4ff669cbe3cc161e418cdf6f55
Vimal Jay <j.vimal@outlook.com>
2018-07-29 23:44:17 +0530

    Removed amazon creds

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -127,20 +127,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-#Using AWS for static files
-AWS_ACCESS_KEY_ID = 'AKIAIEYSRSZ3IZSR65OQ'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'vimal-django-static'
-AWS_S3_CUSTOM_DOMAIN = '{}.s3.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME)
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_LOCATION = 'static'
-
-STATIC_URL = 'https://{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'Portfolio.storage_backends.MediaStorage'
-
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
