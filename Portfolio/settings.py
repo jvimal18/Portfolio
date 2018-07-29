63700f5226d19d38b42b1c02a83a1cd644e95c31
Vimal Jay <j.vimal@outlook.com>
2018-07-29 23:39:30 +0530

    Added S3 support

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'job.apps.JobConfig',
     'personals.apps.PersonalsConfig',
 ]
@@ -122,10 +123,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = 'media/'
+#Using AWS for static files
+AWS_ACCESS_KEY_ID = 'AKIAIEYSRSZ3IZSR65OQ'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'vimal-django-static'
+AWS_S3_CUSTOM_DOMAIN = '{}.s3.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME)
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATIC_URL = 'https://{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'Portfolio.storage_backends.MediaStorage'
+
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
--- job/models.py
+++ job/models.py
@@ -5,3 +5,6 @@ class Job(models.Model):
     title = models.CharField(max_length=40)
     summary = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/', default='images/default.jpg')
+
+    def __str__(self):
+        return 'Job : ' + self.title
